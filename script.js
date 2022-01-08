2023889e0a3a062e28ebdcbf8d8f4e7d56dfd84a
Mohamed Safouan Besrour <besrour_2010@live.com>
2022-01-08 15:46:01 +0100

    imprv design

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ spans[0].innerHTML = wallet.publicKey.toString()
 spans[1].innerHTML = Base58.encode(wallet.secretKey)
 new QRCode(document.getElementById("qrcode-public"), {
 	text: wallet.publicKey.toString(),
-    colorDark : "#000000",
-	colorLight : "#ffffff",
+    colorDark : "#ffffff",
+	colorLight : "#9945FF",
 	correctLevel : QRCode.CorrectLevel.H
 });
 new QRCode(document.getElementById("qrcode-private"), {
 	text: Base58.encode(wallet.secretKey),
-    colorDark : "#000000",
-	colorLight : "#ffffff",
+    colorDark : "#ffffff",
+	colorLight : "#14F195",
 	correctLevel : QRCode.CorrectLevel.H
 });
